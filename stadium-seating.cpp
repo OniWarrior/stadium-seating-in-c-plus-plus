fa41064cb525a5699e31f0e415a9038cade348f4
Stephen Aranda <aranda.stephen88@gmail.com>
2022-08-11 19:25:04 -0500

    Added comments of author

---
+++ stadium-seating.cpp
@@ -0,0 +1,6 @@
+// Author: Stephen Aranda
+// Date: 8/11/2022
+// File Name: stadium-seating.cpp
+// Description: A simple program that displays the sales
+// earned from different classes of seats at a softball game
+
